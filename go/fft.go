daa4094e37a1b7d45d223696a2c2bc5a9a02900a
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2013-04-22 12:09:21 -0400

    FFT basic test implementation
    Signed-off-by:  <adarshakb@gmail.com>

--- go/fft.go
+++ go/fft.go
@@ -6,5 +6,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world ",math.Pi)
+	fmt.Println("Hello world ",math.E)
+
+	var N int = 100
+	
+	x := make([]float64,N)
+	for i := 0; i < len(x); i++ {
+		x[i] = 1
+	}
+	ans := make ([] float64 , N)
+
+	for k := 0; k < N; k++ {
+		var c float64 = (-2.0 * math.Pi * float64(k)) / float64(N)
+		for i := 0; i < N; i++ {
+			ans[k] += x[i] * math.Pow(math.E,c*float64(i))
+		}
+	}
+
+	fmt.Println(ans)
 }
